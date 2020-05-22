986f3a3e46ba8ce3bbbb7feee2a9436b269579ab
Kris Lafrance <kris.lafrance6067@gmail.com>
2020-05-21 19:07:14 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# <README Template>
+
+Written by Kris Lafrance
+
+Uses Javascript, 
+
+# Features
+
+-
+
+# Run
+
+```sh
+git clone https://github.com/Ero6067/<repo>.git
+```
+
+Install dependancies
+
+```sh
+npm install
+```
+
+Run App
+
+```sh
+npm start
+```
---
+++ app.js
@@ -0,0 +1,22 @@
+//* Modal (Traversy Media)
+
+const modal = document.getElementById("simpleModal");
+const modalBtn = document.getElementById("modal-btn");
+const closeBtn = document.getElementsByClassName("close-btn")[0];
+
+const openModal = () => {
+	modal.style.display = "block";
+};
+
+const closeModal = () => {
+	modal.style.display = "none";
+};
+const clickOutside = (e) => {
+	if (e.target == modal) {
+		modal.style.display = "none";
+	}
+};
+
+modalBtn.addEventListener("click", openModal);
+closeBtn.addEventListener("click", closeModal);
+window.addEventListener("click", clickOutside);
